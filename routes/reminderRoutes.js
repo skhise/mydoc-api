c450390f3a59ca8d0ca90cd13b19a8cdda05a601
Vaibhav <vaibhav@sspectra.net>
2025-04-08 23:40:35 +0530

    modify user api and wrote API for reminder

--- routes/reminderRoutes.js
+++ routes/reminderRoutes.js
@@ -1,13 +1,24 @@
 import express from "express";
-import { listReminders, registerReminder } from "../controllers/reminderController.js";
+import { 
+    getAllReminder, 
+    createReminder,
+    getReminderByUserID,
+    getReminderByReminderID,
+    deleteReminderByID,
+    updateReminderByID
+} from "../controllers/reminderController.js";
 
 import { authenticateToken } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.use(authenticateToken);
-// Register User
-router.post("/add", registerReminder);
-router.post("/list", listReminders);
+
+router.post("/createReminder", createReminder);
+router.get("/getAllReminder", getAllReminder);
+router.get("/getReminderByUserID/:created_by", getReminderByUserID);
+router.get("/getReminderByReminderID/:id", getReminderByReminderID);
+router.get("/updateReminderByID/:id", updateReminderByID);
+router.get("/deleteReminderByID/:id", deleteReminderByID);
 
 
 export default router;
